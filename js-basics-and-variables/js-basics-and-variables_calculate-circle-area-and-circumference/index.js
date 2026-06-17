65aa21f32ed0d1d68d3b58027440b0a19e047621
SaskiaWeiß1234 <saskia.weiss@bstableau.de>
2026-06-17 10:45:53 +0200

    initailised circle variables

--- js-basics-and-variables/js-basics-and-variables_calculate-circle-area-and-circumference/index.js
+++ js-basics-and-variables/js-basics-and-variables_calculate-circle-area-and-circumference/index.js
@@ -1,15 +1,20 @@
 console.clear();
 
+
 /*
 How can you calculate the area and circumference of a pizza - oops, I mean a circle? Let`s find out!
-
+let circleRadius = 5;
 1. Initialize a variable to represent the radius of the circle. For example, you can set `radius` to a specific value, like 5.
-
+let circleRadius = 5;
 2. Calculate the area of the circle using the formula `Math.PI * radius ** 2`, and store the result in a variable called `circleArea`.
-
+let circleArea = Math.PI * circleRadius ** 2;
 3. Calculate the circumference of the circle using the formula `2 * Math.PI * radius`, and store the result in a variable called `circumference`.
-
+let circumference = 2 * Math.PI * circleRadius;
 4. Display the results by logging them to the console. Use `console.log` to print the area and circumference.
-
+console.log(circleRadius, circleArea, circumference)
 5. Run your JavaScript program to see the calculated area and circumference of the circle with the specified radius.
 */
+let circleRadius = 5;
+let circleArea = Math.PI * circleRadius ** 2;
+let circumference = 2 * Math.PI * circleRadius;
+console.log(circleRadius, circleArea, circumference);
--- js-basics-and-variables/js-basics-and-variables_console-methods/index.js
+++ js-basics-and-variables/js-basics-and-variables_console-methods/index.js
@@ -3,19 +3,19 @@ console.clear();
 // The exercise starts here!
 
 // For reference, check out all console methods on MDN: https://developer.mozilla.org/en-US/docs/Web/API/console
-
+let coding = "Happy Coding!";
 // 1. Store the sentence 'Happy coding!' in a variable named 'coding'.
 
 // 2. Display the variable 'coding' as a message in the console.
-
+conxole.log(coding);
 // 3. Store the sentence 'Happy hacking!' in a variable named 'hacking'.
-
+let hacking = "Happy Hacking!";
 // 4. Display the variable 'hacking' as a message in the console.
-
+console.log(hacking);
 // 5. Which method is used to clear the console? Use it to tidy up the console.
-
+console.clear();
 // 6. Store the text 'This is an error!' in a variable 'error'.
-
+let error = "This is an error!"
 // 7. Which method is used to display an error message in the console? Use it do display the error message in the console.
-
+console.log(error);
 // Well done!

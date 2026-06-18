24112031892a95fda2cb8ad2c68f276edf4bc17c
SaskiaWeiß1234 <saskia.weiss@bstableau.de>
2026-06-18 11:30:19 +0200

    added darkmode functions to the darkmode-challenge

--- js-DOM-and-events/js-dom-and-events_dark-mode/js/index.js
+++ js-DOM-and-events/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,12 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const addButton = document.querySelector('[data-js="dark-mode-button"]');
+addButton.addEventListener("click", () => { bodyElement.className = "dark"});
+const removeButton = document.querySelector('[data-js="light-mode-button"]');
+removeButton.addEventListener("click", () => {bodyElement.classList.remove("dark")});
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+toggleButton.addEventListener("click", () => {bodyElement.classList.toggle("dark")});
+
+
